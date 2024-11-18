06c96282199fb40e14bec2fc2eb1630ee55ac476
vikram899 <vikramdeshmukh899@gmail.com>
2024-11-18 19:55:25 +0530

    Setup react router

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1>Hello from react</h1>;
-}
-
-export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <h1>Hello from react</h1>;
+}
+
+export default HomePage;
